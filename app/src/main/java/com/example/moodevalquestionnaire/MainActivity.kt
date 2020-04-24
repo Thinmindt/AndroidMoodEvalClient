2ae82f428c7ff68a251a6497c9059b07da4c3754
Greg Harms <slwriotcontrol@gmail.com>
2020-04-24 16:12:19 -0600

    Create day functionality hooked up to submit button. Also checks database on boot and before creating day.

--- app/src/main/java/com/example/moodevalquestionnaire/MainActivity.kt
+++ app/src/main/java/com/example/moodevalquestionnaire/MainActivity.kt
@@ -4,20 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.RadioButton
+import android.widget.TextView
 import android.widget.Toast
-import com.apollographql.apollo.ApolloCall
-import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.api.CustomTypeAdapter
-import com.apollographql.apollo.api.CustomTypeValue
-import com.apollographql.apollo.api.Response
-import com.apollographql.apollo.exception.ApolloException
-import com.example.moodevalquestionnaire.type.CustomType
 import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.OkHttpClient
-import java.text.ParseException
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.ZoneId
 import java.util.*
-import java.util.concurrent.TimeUnit
+import java.util.Date.from
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,63 +19,55 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val network = Network()
+        val localDateYesterday = LocalDate.now().minusDays(2)
+        val yesterday:Date = getYesterday()
 
-
-        val GRAPHQL_DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd", Locale.US)
-
-        val customTypeAdapterDate = object: CustomTypeAdapter<Date> {
-            override fun encode(value: Date): CustomTypeValue<*> {
-                return CustomTypeValue.GraphQLString(GRAPHQL_DATE_FORMAT.format(value))
-            }
-            override fun decode(value: CustomTypeValue<*>): Date {
-                return try {
-                    GRAPHQL_DATE_FORMAT.parse(value.value.toString())
-                } catch (e: ParseException) {
-                    throw RuntimeException(e)
-                }
-            }
+        val date: Date = SimpleDateFormat("yyyy-MM-dd").parse("2020-04-15")
+        network.queryDayByDate(date) { lambResponse ->
+            val response = lambResponse?.moodStr.toString()
+            Log.i("MAIN", "query result = $response")
+            response
         }
 
-        val apolloClient = ApolloClient.builder()
-            .serverUrl("http://192.168.50.9:8080/graphql")
-            .okHttpClient(
-                OkHttpClient.Builder()
-                    .connectTimeout(30, TimeUnit.SECONDS)
-                    .writeTimeout(30, TimeUnit.SECONDS)
-                    .readTimeout(30, TimeUnit.SECONDS)
-                    .build()
-            )
-            .addCustomTypeAdapter(CustomType.NAIVEDATE, customTypeAdapterDate)
-            .build()
-
-
-        val date: Date = SimpleDateFormat("yyyy-MM-dd").parse("2020-04-20")
-        val dayByDateQuery = DayByDateQuery(date)
-
-        val tag = "Main Activity"
-
-        val callbacks = object: ApolloCall.Callback<DayByDateQuery.Data>() {
-            public override fun onResponse(response: Response<DayByDateQuery.Data>) {
-                Log.i(tag, "Got ${response.data?.getDayByDate?.moodStr.toString()}.")
-            }
-
-            public override fun onFailure(e: ApolloException) {
-                Log.i(tag, "Error: ${e.message}")
-            }
-        }
-
-        apolloClient.query(
-            dayByDateQuery
-        )
-            .enqueue(callbacks)
+        checkYesterday(network, yesterday)
 
         button_submit.setOnClickListener {
             val id: Int = mood_choice.checkedRadioButtonId
             if (id != -1) {
                 val mood: RadioButton = findViewById(id)
-                Toast.makeText(this@MainActivity, "You chose ${mood.text}.", Toast.LENGTH_SHORT).show()
+                val moodId = moodTextToEnum(mood.text as String)
 
+                network.createDay(yesterday, moodId)
+                Toast.makeText(this@MainActivity, "You chose ${mood.text}: $moodId.", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun moodTextToEnum(mood: String): Int {
+        var enum = -1
+        when (mood) {
+            "Terrible" -> enum = 1
+            "Bad" -> enum = 2
+            "Normal" -> enum = 3
+            "Good" -> enum = 4
+            "Marvelous" -> enum = 5
+        }
+        return enum
+    }
+
+    private fun checkYesterday(network: Network, yesterday: Date) {
+        network.queryDayByDate(yesterday) { lambResponse ->
+            if (lambResponse != null) {
+                val topMessage = findViewById<TextView>(R.id.top_message)
+                topMessage.text = "You have already entered data for yesterday."
+            }
+            "Complete"
+        }
+    }
+
+    private fun getYesterday(): Date {
+        val localDateYesterday = LocalDate.now().minusDays(2)
+        return from(localDateYesterday.atStartOfDay(ZoneId.systemDefault()).toInstant())
     }
 }
--- app/src/main/java/com/example/moodevalquestionnaire/Network.kt
+++ app/src/main/java/com/example/moodevalquestionnaire/Network.kt
@@ -1,10 +1,108 @@
 package com.example.moodevalquestionnaire
 
 import android.content.Context
+import android.util.Log
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.CustomTypeAdapter
+import com.apollographql.apollo.api.CustomTypeValue
+import com.apollographql.apollo.api.Input
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.example.moodevalquestionnaire.type.CustomType
+import com.example.moodevalquestionnaire.type.NewDay
+import okhttp3.OkHttpClient
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class Network {
-    fun setApolloClient (context: Context){
+    private val GRAPHQL_DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+    private val TAG = "NETWORK"
 
+    var apolloClient: ApolloClient
 
+    init {
+        apolloClient = setApolloClient()
+    }
+
+    data class ResponseString(var result: String? = null)
+
+    fun queryDayByDate(date: Date, responseCallback: (response: DayByDateQuery.GetDayByDate?) -> String ): String? {
+        var queryResponse: String? = null
+        val dayByDateQuery = DayByDateQuery(date)
+        val callbacks = object: ApolloCall.Callback<DayByDateQuery.Data>() {
+            override fun onResponse(response: Response<DayByDateQuery.Data>) {
+                Log.i(TAG, "Got ${response.data?.getDayByDate?.moodStr.toString()}.")
+                queryResponse = responseCallback(response.data?.getDayByDate)
+            }
+
+            override fun onFailure(e: ApolloException) {
+                Log.i(TAG, "Error: ${e.message}")
+            }
+        }
+        apolloClient.query(
+            dayByDateQuery
+        )
+            .enqueue(callbacks)
+        return queryResponse
+    }
+
+    fun createDay(date: Date, mood: Int?) {
+        val moodInput: Input<Int> = Input.fromNullable(mood)
+        Log.i(TAG, "Mood Input = ${moodInput.value}, Mood = $mood")
+
+        //Check if day exists
+        queryDayByDate(date) { response ->
+            Log.i(TAG, "Mood value for date given = ${response?.moodStr.toString()}")
+            // It doesn't exist
+            if (response == null) {
+                val data = NewDay(date = date, moodId = moodInput)
+                val createDay = CreateDayMutation(data)
+
+                val callbacks = object: ApolloCall.Callback<CreateDayMutation.Data>() {
+                    override fun onResponse(response: Response<CreateDayMutation.Data>) {
+                        Log.i(TAG, "Got ${response.data?.createDay?.moodStr.toString()}.")
+                    }
+
+                    override fun onFailure(e: ApolloException) {
+                        Log.i(TAG, "Error: ${e.message}")
+                    }
+                }
+
+                apolloClient.mutate(createDay)
+                    .enqueue(callbacks)
+            }
+            "Complete"
+        }
+    }
+
+    private fun setApolloClient (): ApolloClient {
+        val customTypeAdapterDate = object : CustomTypeAdapter<Date> {
+            override fun encode(value: Date): CustomTypeValue<*> {
+                return CustomTypeValue.GraphQLString(GRAPHQL_DATE_FORMAT.format(value))
+            }
+
+            override fun decode(value: CustomTypeValue<*>): Date {
+                return try {
+                    GRAPHQL_DATE_FORMAT.parse(value.value.toString())
+                } catch (e: ParseException) {
+                    throw RuntimeException(e)
+                }
+            }
+        }
+
+        return ApolloClient.builder()
+            .serverUrl("http://192.168.50.9:8080/graphql")
+            .okHttpClient(
+                OkHttpClient.Builder()
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .writeTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .build()
+            )
+            .addCustomTypeAdapter(CustomType.NAIVEDATE, customTypeAdapterDate)
+            .build()
     }
 }
